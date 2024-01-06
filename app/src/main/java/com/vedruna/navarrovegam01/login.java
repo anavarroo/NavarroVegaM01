1a210b096b9cef1b3ba19c068679f6f81c15a5fe
Alberto Navarro <milumarchena@gmail.com>
2024-01-06 18:45:39 +0100

    EJ02

---
+++ app/src/main/java/com/vedruna/navarrovegam01/login.java
@@ -0,0 +1,46 @@
+package com.vedruna.navarrovegam01;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.vedruna.navarrovegam01.R;
+
+public class login extends AppCompatActivity {
+
+    private TextView mensaje;
+    private EditText user;
+    private EditText password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        mensaje = findViewById(R.id.ComprobacionLogin);
+
+        user = findViewById(R.id.inputUser);
+        password = findViewById(R.id.inputPassword);
+
+    }
+
+    public void onClick(View view){
+        String username = user.getText().toString();
+        String contraseña = password.getText().toString();
+
+        if (username.equals("admin") && contraseña.equals("admin")){
+            String usuario = username;
+            Intent intent = new Intent(login.this,contador.class);
+            intent.putExtra("usuario",usuario);
+            startActivity(intent);
+
+        } else {
+            mensaje.setText("Usuario o contraseña incorrecta");
+        }
+
+    }
+}
+
